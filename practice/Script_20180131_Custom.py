c9fd6eb1af6049ba5f9424671764b6564f196ce5
takumi yogo <takk@takuminoMacBook-Pro.local>
2018-01-31 22:08:14 +0900

    3rd Commit

--- practice/Script_20180131_Custom.py
+++ practice/Script_20180131_Custom.py
@@ -3,7 +3,7 @@ import bpy
 name = 'TestPanel'
 size = 1
 rows = 2
-columns = 4
+columns = 2
 
 def vert(column, row): return (column * size, row * size, 0)
 
@@ -21,3 +21,18 @@ mesh.from_pydata(verts, [], faces)
 
 obj = bpy.data.objects.new(name, mesh)
 bpy.context.scene.objects.link(obj)
+
+###### for test ############
+v = [x for x in range(3)]
+print(v)
+###### for test2 ############
+w = [(x,y) for x in range(3) for y in range(2)]
+print(w)
+###### for test3 ############
+verts_num = [vert(x, y) for x in range(3) for y in range(2)]
+faces_num = [face(x, y) for x in range(3 -1) for y in range(2 - 1)]
+print('*** verts_num ***')
+print(verts_num)
+print('*** faces_num ***')
+print(faces_num)
+
